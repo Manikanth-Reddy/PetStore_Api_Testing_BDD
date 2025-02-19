bc84088a07643ed7a9c9779ed68bcf2cb0d4c211
Manikanth Reddy <manikanth18100@protonmail.com>
2025-02-19 08:49:35 +0530

    updated

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,47 +1,47 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "da7d3bef-3a22-4b92-a513-c10c56539f3b",
-    "feature": "API Testing",
-    "scenario": "Verify GET API",
-    "start": 1739872891044,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1739872896534,
-    "className": "passed"
-  },
-  {
-    "id": "b180c6b9-29b8-4897-bcec-4b30e0784f95",
+    "id": "2180d656-3caa-4353-997f-5bedd8ba99ed",
     "feature": "API Testing",
     "scenario": "Verify Post API",
-    "start": 1739872896584,
+    "start": 1739934916187,
     "group": 1,
     "content": "",
     "tags": "",
-    "end": 1739872898818,
+    "end": 1739934918582,
     "className": "passed"
   },
   {
-    "id": "876fcdb5-d671-4356-86e1-e4e45c6fe319",
-    "feature": "API Testing",
-    "scenario": "Verify Get with id API",
-    "start": 1739872898839,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1739872900620,
-    "className": "passed"
-  },
-  {
-    "id": "0e67d443-4e50-46b3-863a-7b4a1e8bb1d0",
+    "id": "60b28711-626b-455a-9396-f8d620a6d9af",
     "feature": "API Testing",
     "scenario": "Verify Delete API",
-    "start": 1739872900645,
+    "start": 1739934921553,
     "group": 1,
     "content": "",
     "tags": "",
-    "end": 1739872902646,
+    "end": 1739934922807,
+    "className": "passed"
+  },
+  {
+    "id": "4cbc8aee-a2ad-4391-bb47-79572b1d485e",
+    "feature": "API Testing",
+    "scenario": "Verify Get with id API",
+    "start": 1739934918592,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1739934921547,
+    "className": "passed"
+  },
+  {
+    "id": "c4bba065-9aec-4050-a119-ceb03546ea75",
+    "feature": "API Testing",
+    "scenario": "Verify GET API",
+    "start": 1739934911459,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1739934916175,
     "className": "passed"
   }
 ]);
